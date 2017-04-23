3c8fba00625a1b44581077cd3ee3ebbe608e2912
IamNumberN <phung8128@gmail.com>
2017-04-23 04:24:03 -0500

    wrote cs script for scene change

---
+++ Hackathon/2t2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Triptwo : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Trip 2 Scroll");
+    }
+}
---
+++ Hackathon/2t5.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Tripfive : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Trip 5 Scroll");
+    }
+}
