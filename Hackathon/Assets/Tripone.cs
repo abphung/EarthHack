8080171e3ae1d4e7eb2068979481d97dc18783fd
IamNumberN <phung8128@gmail.com>
2017-04-23 05:00:35 -0500

    added assets folder

---
+++ Hackathon/Assets/Tripone.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Tripone : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Trip 1 Scroll");
+    }
+}
---
+++ Hackathon/Assets/Tripthree.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class Tripthree : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Trip 3 Scroll");
+    }
+}
